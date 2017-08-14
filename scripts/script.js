162a9119cef0610445b424a6e80516c7e75c9d86
James Wood <jamie_wood@ymail.com>
2017-08-14 16:13:11 +0100

    Add image gallery

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 var captionId = 0;
+var fullscreenImage = null;
 
 var captions = [
   "creating cyber surprises",
@@ -18,4 +19,43 @@ function setCaption()
   console.log("setting caption");
   document.getElementById("caption").innerHTML = captions[Math.min(captionId, captions.length - 1)];
   captionId += 1;
+}
+
+function gifStart(image)
+{
+    image.src = image.src.replace(".jpg", ".gif");
+}
+
+function gifStop(image)
+{
+    image.src = image.src.replace(".gif", ".jpg");
+}
+
+function enterFullscreen(image)
+{
+  if (fullscreenImage != null)
+  {
+    fullscreenImage.parentNode.removeChild(fullscreenImage);
+    fullscreenImage = null;
+  }
+
+  fullscreenImage = document.createElement("div");
+  var fullscreenImageChild = document.createElement("img");
+  fullscreenImageChild.src = image.src;
+  fullscreenImageChild.classList.add("fullscreenImageChild");
+  fullscreenImage.appendChild(fullscreenImageChild);
+
+  fullscreenImage.onclick = exitFullscreen;
+  fullscreenImage.classList.add("fullscreenImage");
+
+  document.body.appendChild(fullscreenImage);
+}
+
+function exitFullscreen()
+{
+    if (fullscreenImage != null)
+    {
+      fullscreenImage.parentNode.removeChild(fullscreenImage);
+      fullscreenImage = null;
+    }
 }
