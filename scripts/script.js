e809e31f02ab50bd9cfc7c48945cdc5426e4dc59
Henry <Henwuar@users.noreply.github.com>
2024-09-13 17:25:54 +0100

    Set up image content

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-var captionId = 0;
 var fullscreenImage = null;
 
 var hiddenContainer = document.getElementById('hiddenContent');
@@ -6,24 +5,31 @@ var displayContainer = document.getElementById('displayContent');
 var taglineContainer = document.getElementById('tagline');
 var navigationContainer = document.getElementById('navigation');
 
-var captions = [
-  "creating cyber surprises",
-  "collaborating on cool content",
-  "interactive goop",
-  "that tickles",
-  "stop it",
-  "I said stop.",
-  "I will just leave if you keep doing that.",
-  "Please, I am asking nicely.",
-  "Okay have it your way.",
-  "Goodbye.",
-  "" ]
+const slideTimeSeconds = 5;
 
-function setCaption()
+var games = [
+  "Games",
+  "Interactives"
+ ]
+
+ var adjectivesB =[
+  "Surprise",
+  "Move"
+ ]
+
+ var adjectivesB = [
+  "Delight",
+  "Shake"
+ ]
+
+var slidePositions = []
+
+function randomiseCaption()
 {
   console.log("setting caption");
-  document.getElementById("caption").innerHTML = captions[Math.min(captionId, captions.length - 1)];
-  captionId += 1;
+  document.getElementById("tag2").innerHTML = games[Math.floor(Math.random() * games.length)];
+  document.getElementById("tag4").innerHTML = adjectivesA[Math.floor(Math.random() * adjectivesA.length)];
+  document.getElementById("tag6").innerHTML = adjectivesB[Math.floor(Math.random() * adjectivesB.length)];
 }
 
 function swapContent(contentId, clickedButton)
@@ -47,3 +53,66 @@ function swapContent(contentId, clickedButton)
 
   displayContainer.appendChild(document.getElementById(contentId));
 }
+/*
+function advanceSlides()
+{
+  console.log("Advance!");
+
+  let slideContent = document.getElementsByClassName("slideshow");
+  for (let slideshow = 0; slideshow < slideContent.length; slideshow++) 
+  {
+    const element = slideContent[slideshow];
+    var lastSlide = 0
+    if(slidePositions.length <= slideshow)
+    {
+      slidePositions.push(0);
+    }
+    else
+    {
+      lastSlide = slidePositions[slideshow]
+      slidePositions[slideshow]++;
+    }
+
+    var slides = element.getElementsByClassName("screenshot");
+    if(slides.length <= slidePositions[slideshow])
+    {
+      slidePositions[slideshow] = 0;
+    }
+    
+    if(slides.length == 0)
+    {
+      console.log("no slides!")
+      continue;
+    }
+
+    console.log(slidePositions[slideshow]);
+
+    for(let slide = 0; slide < slides.length; slide++)
+    {
+      console.log(slide);
+      if(slide == slidePositions[slideshow])
+      {
+        console.log("Fade in!");
+        slides[slide].className += " fadeInScreenshot";
+        slides[slide].style.display = "inherit";  
+      }
+      else if(slide == lastSlide)
+      {
+        console.log("Fade out!")
+        slides[slide].className += " fadeOutScreenshot";
+        slides[slide].style.display = "inherit";
+      }
+      else
+      {
+        console.log("hide!")
+        slides[slide].className = slides[slide].className.replace(" fadeOutScreenshot", "");
+        slides[slide].className = slides[slide].className.replace(" fadeInScreenshot", "");
+        slides[slide].display = "none";
+      }
+    }
+
+    setTimeout(advanceSlides, slideTimeSeconds*1000);
+  }
+}
+*/
+//advanceSlides()
